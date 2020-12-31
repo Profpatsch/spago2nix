f793e19afb1cda1b41cc568201712aa957f98c45
Profpatsch <mail@profpatsch.de>
2020-12-31 14:47:45 +0100

    chore(nix): use niv to fix dependencies
    ```
    niv init
    niv add github justinwoo/easy-purescript-nix
    niv add github justinwoo/easy-dhall-nix
    ```
    
    and a bit of shuffling to import the sources in the right way.
    
    default.nix should have the same interface than before, I make sure
    the existing args are still there.

--- shell.nix
+++ shell.nix
@@ -1,42 +1,13 @@
-let
-  easy-dhall-nix = pkgs: import (
-    pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-dhall-nix";
-      rev = "9c4397c3af63c834929b1e6ac25eed8ce4fca5d4";
-      sha256 = "1cbrqfbx29rymf4sia1ix4qssdybjdvw0is9gv7l0wsysidrcwhf";
-    }
-  ) {
-    inherit pkgs;
-  };
-
-  easy-purescript-nix = pkgs: import (
-    pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-purescript-nix";
-      rev = "1ec689df0adf8e8ada7fcfcb513876307ea34226";
-      sha256 = "12hk2zbjkrq2i5fs6xb3x254lnhm9fzkcxph0a7ngxyzfykvf4hi";
-    }
-  ) {
-    inherit pkgs;
-  };
-
-
-in
-
-{ pkgs ? import <nixpkgs> {}
-, dhall-json ? (easy-dhall-nix pkgs).dhall-json-simple
-, spago ? (easy-purescript-nix pkgs).spago
-, purescript ? (easy-purescript-nix pkgs).purescript
-, nodejs ? pkgs.nodejs-10_x
+{ sources ? import ./nix/sources.nix
 }:
 
-pkgs.mkShell {
+let deps = import ./deps.nix { inherit sources; };
+in
+deps.pkgs.mkShell {
   buildInputs = [
-    dhall-json
-    nodejs
-    spago
-    purescript
+    deps.dhall-json
+    deps.pkgs.nodejs
+    deps.spago
+    deps.purescript
   ];
-
 }
